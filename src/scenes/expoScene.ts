327cd84ae5706cb7a0acf38278b99185be25cfdb
MVNC26 <djavuaxebahia@gmail.com>
2024-06-12 17:21:39 -0300

    feat: colisão

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,4 @@
-import { Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, FadeInOut, HiddenEvent, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 import { Player } from "../actors/player";
 
@@ -34,5 +34,24 @@ export class expoScene extends Scene{
         jogador.z = 1 
 
         this.add(jogador)
+
+        //adicionar colisao com objetos
+        let camadaObjestosColisores = tiledMap.getObjectLayers("objetosColisores")[0]
+        
+        camadaObjestosColisores.objects.forEach(objeto => {
+
+          const objetoAtual = new Actor ({
+            name: objeto.name,
+            x: objeto.x + offsetX + (objeto.tiledObject.width! / 2),
+            y: objeto.y + offsetY + (objeto.tiledObject.height! /2),
+
+            width: objeto.tiledObject.width,
+            height: objeto.tiledObject.height,
+            collisionType: CollisionType.Fixed
+          })
+
+          this.add(objetoAtual)
+
+        })
     }
 }
